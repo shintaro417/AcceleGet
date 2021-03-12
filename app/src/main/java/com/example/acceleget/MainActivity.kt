fd52e97ce9dd36f722a24e621d2ffd5a025b6caf
shintaro417 <shintaro11417@gmail.com>
2021-03-12 11:25:15 +0900

    second Commit

--- app/src/main/java/com/example/acceleget/MainActivity.kt
+++ app/src/main/java/com/example/acceleget/MainActivity.kt
@@ -5,19 +5,43 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.acceleget.databinding.ActivityMainBinding
 
 /**
  * SensorEventListener:加速度センサーの実装に必要
  */
 class MainActivity : AppCompatActivity(),SensorEventListener {
+    //onCreateメソッドで初期化したい変数はlateinit修飾子を使う
     private lateinit var binding: ActivityMainBinding
+    //閾値
+    private var threshold: Float = 10f
+    //加速度センサーのZ軸の値を記憶する
+    private var oldValue: Float = 0f
+    private lateinit var cameraManager:CameraManager
+    private var cameraID:String? = null
+    //LEDがONかOffか
+    private var lightOn:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        //トーチモードが変更されたときの処理を登録
+        cameraManager.registerTorchCallback(object : CameraManager.TorchCallback(){
+            //トーチモードが変更されたときに呼び出されるメソッド
+            override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
+                super.onTorchModeChanged(cameraId, enabled)
+                cameraID = cameraId
+                lightOn = enabled
+            }
+        }, Handler(Looper.myLooper()!!))
     }
 
     /**
@@ -26,7 +50,13 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
         if(event == null)return
         if(event.sensor.type == Sensor.TYPE_ACCELEROMETER){
-            binding.txt01.text="x軸:${event.values[0]}\nY軸:${event.values[1]}\nZ軸:${event.values[2]}"
+            //Android端末が倒れたと判断したかどうか判定する
+            val zDiff = Math.abs(event.values[2] - oldValue)
+            //Z軸の値と保存しておいた値の差が閾値を超えた場合に実施
+            if(zDiff > threshold){
+                torchOn()
+            }
+            oldValue = event.values[2]
         }
     }
 
@@ -50,5 +80,32 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         //リスナーの登録を解除する。
         sensorManager.unregisterListener(this)
+
+        if(cameraID != null){
+            try {
+                if (lightOn){
+                    cameraManager.setTorchMode(cameraID!!,false)
+                }
+            }catch (e:CameraAccessException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    /**
+     * LEDを点灯、消灯
+     */
+    private fun torchOn(){
+        if(cameraID != null){
+            try {
+                if(!lightOn){
+                    cameraManager.setTorchMode(cameraID!!,true)
+                }else{
+                    cameraManager.setTorchMode(cameraID!!,false)
+                }
+            }catch (e:CameraAccessException){
+                e.printStackTrace()
+            }
+        }
     }
 }
